b64dea20d8a7a430f1173aaa1da0c474ce983062
John Baylies <jbaylies@gmail.com>
2022-07-15 22:23:07 -0400

    add LED bell max proj

--- Start-the-Rig.sh
+++ Start-the-Rig.sh
@@ -10,7 +10,7 @@
 
 # System Preferences > Keyboard > Shortcuts > Mission Control > enable Switch to Desktop
 #
-# right-click Totalmix & Ableton in dock > Options > Assign To > None
+# right-click apps in dock > Options > Assign To > None
 #
 # https://eastmanreference.com/complete-list-of-applescript-key-codes
 
@@ -18,16 +18,22 @@ osascript -e 'tell application "System Events" to key code 18 using {control dow
 osascript -e 'tell application "Totalmix" to activate' 
 sleep 1
 
-osascript -e 'tell application "System Events" to key code 20 using {control down}' # Desktop 3
-osascript -e 'tell application "Max" to activate' # Assign To > Desktop on Display 2
+osascript -e 'tell application "System Events" to key code 19 using {control down}' # Desktop 2
+osascript -e 'tell application "Max" to activate'
 sleep 2
 
 open ~/Music/_repo/sousastep/Sousastep\ XII/Sousastep\ XII.maxproj
-sleep 1
+sleep 3
+
+osascript -e 'tell application "System Events" to key code 20 using {control down}' # Desktop 3
+
+open ~/Music/_repo/sousastep/Sousastep\ LEDs\ amplitude\ effects/Sousastep\ LEDs\ amplitude\ effects.maxproj
+sleep 3
 
 osascript -e 'tell application "System Events" to key code 21 using {control down}' # Desktop 4
 osascript -e 'tell application "Ableton Live 11 Suite" to activate'
-sleep 0.1
+sleep 1
 
 open ~/Music/_repo/sousastep/Sousastep\ XII\ Project/Sousastep\ XII.als
 sleep 5
+
