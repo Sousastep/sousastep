7aaf3b45fc59145de861e871ebf56448d8f9d50b
John Baylies <jbaylies@gmail.com>
2022-08-24 23:25:25 -0400

    rename project files

--- Start-the-Rig.sh
+++ Start-the-Rig.sh
@@ -22,18 +22,18 @@ osascript -e 'tell application "System Events" to key code 19 using {control dow
 osascript -e 'tell application "Max" to activate'
 sleep 2
 
-open ~/Music/_repo/sousastep/Sousastep\ XII/Sousastep\ XII.maxproj
+open ~/Music/_repo/sousastep/Sousastep\ Audio\ FX/Sousastep\ Audio\ FX.maxproj
 sleep 3
 
 osascript -e 'tell application "System Events" to key code 20 using {control down}' # Desktop 3
 
-open ~/Music/_repo/sousastep/Sousastep\ LEDs\ amplitude\ effects/Sousastep\ LEDs\ amplitude\ effects.maxproj
+open ~/Music/_repo/sousastep/Sousastep\ Visual\ FX/Sousastep\ Visual\ FX.maxproj
 sleep 3
 
 osascript -e 'tell application "System Events" to key code 21 using {control down}' # Desktop 4
 osascript -e 'tell application "Ableton Live 11 Suite" to activate'
 sleep 1
 
-open ~/Music/_repo/sousastep/Sousastep\ XII\ Project/Sousastep\ XII.als
+open ~/Music/_repo/sousastep/Sousastep\ Live\ Project/Sousastep\ Live.als
 sleep 5
 
